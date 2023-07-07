68de3bcfb437dc7f8e3bdf4890ebc5e670ccd378
betlcelik <celikbetul313@gmail.com>
2023-07-07 10:41:45 +0300

    7 Temmuz

---
+++ Staj-Vortex-main/SpotifyClone/SpotifyCloneBusiness/abstracts/IPremiumUserMembershipControlService.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using SpotifyClone.Core.Utilities.Results.Concretes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpotifyClone.Business.abstracts
+{
+    public interface IPremiumUserMembershipControlService
+    {
+        public IResult ChechkIsMembershipUpToEnd(int userId);
+        public IResult CheckIsMembershipFinished(int userId);
+    }
+}
---
+++ Staj-Vortex-main/SpotifyClone/SpotifyCloneBusiness/concretes/PremiumUserMembershipControllerManager.cs
@@ -0,0 +1,38 @@
+﻿
+using SpotifyClone.Business.abstracts;
+using SpotifyClone.Core.Utilities.Results.Abstract;
+using SpotifyClone.Core.Utilities.Results.Concretes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpotifyClone.Business.concretes
+{
+    public class PremiumUserMembershipControllerManager : IPremiumUserMembershipControlService
+    {
+        private IMembershipService _membershipService;
+        public PremiumUserMembershipControllerManager(IMembershipService membershipService)
+        {
+            _membershipService = membershipService;
+        }
+        public IResult ChechkIsMembershipUpToEnd(int userId)
+        {
+            DateTime currentDateTime = DateTime.Now.ToUniversalTime().Date;
+            var membership=_membershipService.GetByUserId(userId).Data.FirstOrDefault();
+            if(membership.membershipTypeId != 1 && currentDateTime.AddDays(1) == membership.endDate)
+            {
+                return new SuccessResult("Today is the last day of your membership.You may wish to renew it.");
+            }
+            return new SuccessResult();
+        }
+
+        public IResult CheckIsMembershipFinished(int userId)
+        {
+            DateTime currentDateTime = DateTime.Now.ToUniversalTime().Date;
+            //var membership=_membershipService.
+            return null;
+        }
+    }
+}
